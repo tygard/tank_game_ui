0bbce3f3a1e78d47588a034aa3be7dd4c7bd0785
Ryan Ray <ryan314r@gmail.com>
2024-03-24 18:09:20 +0000

    Add coordinates

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,7 +5,7 @@ import {Board} from "./ui/game_state/board.jsx";
 function App() {
     const entities = [
         { type: "tank", name: "Tank 1", position: { x: 0, y: 0 }, lives: 3, range: 2, gold: 5, actions: 1 },
-        { type: "tank", name: "Tank 2", position: { x: 5, y: 5 }, lives: 1, range: 7, gold: 3, actions: 5 },
+        { type: "tank", name: "Tank 2", position: { x: 3, y: 4 }, lives: 1, range: 7, gold: 3, actions: 5 },
         { type: "wall", durability: 3, position: { x: 7, y: 8 } },
         { type: "wall", durability: 2, position: { x: 3, y: 5 } },
         { type: "wall", durability: 1, position: { x: 6, y: 2 } },
@@ -13,7 +13,7 @@ function App() {
         { type: "wall", durability: 3, position: { x: 9, y: 3 } },
     ];
 
-    const tiles = [
+    const areaOfEffects = [
         { type: "gold-mine", spaces: [
             { x: 3, y: 4 },
             { x: 3, y: 3 },
@@ -25,7 +25,7 @@ function App() {
     ];
 
     return (
-        <Board width={10} height={9} entities={entities} tiles={tiles}></Board>
+        <Board width={10} height={9} entities={entities} areaOfEffects={areaOfEffects}></Board>
     );
 }
 
--- frontend/src/ui/game_state/board.jsx
+++ frontend/src/ui/game_state/board.jsx
@@ -14,10 +14,10 @@ function buildEmptyBoard(width, height) {
     return board;
 }
 
-function buildBoard(width, height, entities, tiles) {
+function buildBoard(width, height, entities, areaOfEffects) {
     let board = {
         entities: buildEmptyBoard(width, height),
-        tiles: buildEmptyBoard(width, height),
+        areaOfEffects: buildEmptyBoard(width, height),
     };
 
     for(const entity of entities) {
@@ -26,28 +26,41 @@ function buildBoard(width, height, entities, tiles) {
         }
     }
 
-    for(const tile of tiles) {
-        for(const space of tile.spaces) {
-            board.tiles[space.y][space.x] = tile;
+    for(const areaOfEffect of areaOfEffects) {
+        for(const space of areaOfEffect.spaces) {
+            board.areaOfEffects[space.y][space.x] = areaOfEffect;
         }
     }
 
     return board;
 }
 
-export function Board({ width, height, entities, tiles }) {
-    // Track which tile entities we've rendered a label for so we only render them 1 time (max)
+export function Board({ width, height, entities, areaOfEffects }) {
+    // Track which areaOfEffect entities we've rendered a label for so we only render them 1 time (max)
     let renderedAreaOfEffects = new Set();
 
-    const boardState = buildBoard(width, height, entities, tiles)
+    const boardState = buildBoard(width, height, entities, areaOfEffects)
+
+    let letters = [<Tile className="board-space-coordinate"></Tile>];
+    const a = "A".charCodeAt(0);
+    for(let x = 0; x < width; ++x) {
+        const letter = String.fromCharCode(a + x);
+        letters.push(<Tile className="board-space-coordinate">{letter}</Tile>);
+    }
 
     return (
         <div className="game-board">
+            <div className="game-board-row">
+                {letters}
+            </div>
             {boardState.entities.map((row, y) => (
                 <div className="game-board-row">
+                    <Tile className="board-space-coordinate">{y + 1}</Tile>
                     {row.map((space, x) => {
-                        return <Space space={space} tile={boardState.tiles[y][x]}
-                            renderedAreaOfEffects={renderedAreaOfEffects}></Space>;
+                        const disabled = false;//!((3 <= x && x <= 5) && (3 <= y && y <= 5));
+
+                        return <Space space={space} areaOfEffect={boardState.areaOfEffects[y][x]}
+                            renderedAreaOfEffects={renderedAreaOfEffects} disabled={disabled}></Space>;
                     })}
                 </div>
             ))}
@@ -55,34 +68,38 @@ export function Board({ width, height, entities, tiles }) {
     )
 }
 
-function Space({ space, tile, renderedAreaOfEffects }) {
+function Space({ space, areaOfEffect, renderedAreaOfEffects, disabled }) {
     const type = space && space.type;
-    const tileType = tile && tile.type;
+    const areaOfEffectType = areaOfEffect && areaOfEffect.type;
     let entity = null;
 
     // Try to place an entity in this space
     if(type == "tank") {
-        entity = <Tank tank={space} tile={tile}></Tank>;
+        entity = <Tank tank={space} areaOfEffect={areaOfEffect}></Tank>;
     }
     else if(type == "wall") {
-        entity = <Wall wall={space} tile={tile}></Wall>;
+        entity = <Wall wall={space} areaOfEffect={areaOfEffect}></Wall>;
     }
-    // Try to place a tile in this space
-    else if(tileType == "gold-mine" && !renderedAreaOfEffects.has(tile)) {
-        renderedAreaOfEffects.add(tile);
-        entity = <GoldMineLabel mine={tile}></GoldMineLabel>;
+    // Try to place a areaOfEffect in this space
+    else if(areaOfEffectType == "gold-mine" && !renderedAreaOfEffects.has(areaOfEffect)) {
+        renderedAreaOfEffects.add(areaOfEffect);
+        entity = <GoldMineLabel mine={areaOfEffect}></GoldMineLabel>;
     }
 
     return (
-        <Tile tile={tile}>
+        <Tile areaOfEffect={areaOfEffect} disabled={disabled}>
             {entity}
         </Tile>
     );
 }
 
-function Tile({ className = "", children, tile } = {}) {
-    if(tile && tile.type == "gold-mine") {
+function Tile({ className = "", children, areaOfEffect, disabled } = {}) {
+    if(areaOfEffect && areaOfEffect.type == "gold-mine") {
         className += " board-space-gold-mine";
+    }
+
+    if(disabled) {
+        className += " board-space-disabled";
     }
 
     return (
